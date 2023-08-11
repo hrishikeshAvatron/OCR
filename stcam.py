e9ef7db6029325dc186b8c33bbeca51558f58218
hrishikeshAvatron <123436316+hrishikeshAvatron@users.noreply.github.com>
2023-08-11 16:49:50 +0530

    Add files via upload

---
+++ stcam.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import cv2
+import numpy as np
+from streamlit_back_camera_input import back_camera_input
+
+#if __name__ == '__main__':
+image = back_camera_input()
+if image:
+    st.image(image)
